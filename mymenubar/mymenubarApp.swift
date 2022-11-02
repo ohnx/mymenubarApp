20e8159e562b5f2dbd37b94a91efc587fce98ee4
Mason <me@masonx.ca>
2022-11-02 15:05:18 -0400

    Initial Commit

---
+++ mymenubar/mymenubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  mymenubarApp.swift
+//  mymenubar
+//
+//  Created by Mason on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct mymenubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
