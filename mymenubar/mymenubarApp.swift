b0589f8392d03fd459b99190d476f89b2192ef2d
Mason <me@masonx.ca>
2022-11-02 22:02:53 -0400

    update with code

--- mymenubar/mymenubarApp.swift
+++ mymenubar/mymenubarApp.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 @main
 struct mymenubarApp: App {
+    @State var scripts : [URL]
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("MMBA", systemImage: "hammer") {
+            VStack {
+                ForEach (scripts, id: \.self) { script_url in
+                    Button("\(script_url.lastPathComponent)") {
+                        do {
+                            try Process.run(script_url, arguments: []) {_ in
+                                
+                            }
+                        } catch {
+                            
+                        }
+                    }
+                }
+            }
+            Divider()
+            Button("Refresh") {
+                print("TODO")
+            }
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+    
+    init() {
+        do {
+            let documentURL = FileManager.default.homeDirectoryForCurrentUser
+            let path = documentURL.appendingPathComponent(".config").appendingPathComponent("mmba").absoluteURL
+            scripts = try FileManager.default.contentsOfDirectory(at: path, includingPropertiesForKeys: nil, options: [])
+        } catch {
+            print(error.localizedDescription)
+            scripts = []
         }
     }
 }
